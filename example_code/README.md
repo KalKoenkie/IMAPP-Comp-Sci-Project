381c738a881ab805a3ba58fcaadbd7c9dac1c035
KalKoenkie <134587796+KalKoenkie@users.noreply.github.com>
2025-12-04 10:42:15 +0100

    Added the professors files useful for our project

---
+++ example_code/README.md
@@ -0,0 +1,27 @@
+# How to build
+
+If the build system `ninja` is available:
+
+```shell
+cmake -S . -B build -G"Ninja Multi-Config"
+cmake --build build --config Debug
+cmake --build build --config Release
+```
+
+The build artifacts (e.g. executables) are in the build/Debug and build/Release
+directories, respectively.
+
+Otherwise, just rely on the default generator (typically Makefiles):
+
+```shell
+cmake -S . -B build-d -DCMAKE_BUILD_TYPE=Debug
+cmake --build build-d
+```
+
+```shell
+cmake -S . -B build-o -DCMAKE_BUILD_TYPE=Release
+cmake --build build-o
+```
+
+The build artifacts (e.g. executables) are in the build-d and build-o
+directories, respectively.
---
+++ example_code/pack.cpp
@@ -0,0 +1,63 @@
+#include <algorithm>
+#include <iostream>
+#include <random>
+#include <vector>
+#include "benchmark.hpp"
+
+struct P1
+{
+  int a;
+  double b;
+  int c;
+};
+
+struct P2
+{
+  int a;
+  int c;
+  double b;
+};
+
+auto make_vector(int N)
+{
+  std::random_device rd;
+  std::default_random_engine eng{rd()};
+
+  std::uniform_real_distribution<> dist{-1., 1.};
+
+  std::vector<P1> result;
+  result.reserve(N);
+  std::generate_n(std::back_inserter(result), N, [&] {
+    return P1{1, dist(eng), 2};
+  });
+
+  return result;
+}
+
+int main()
+{
+  std::cout << "sizeof(P1): " << sizeof(P1) << '\n';
+  int const N{100'000'000};
+  auto const v = make_vector(N);
+  auto v1 = v;
+  std::cout << "sorting a vector<P1> in " << benchmark([&] {
+    std::sort(
+      v1.begin(),
+      v1.end(),  //
+      [](auto& e1, auto& e2) { return e1.b < e2.b; }
+    );
+  }).count() << " s\n";
+
+  std::cout << "sizeof(P2): " << sizeof(P2) << '\n';
+  std::vector<P2> v2;
+  v2.reserve(v.size());
+  std::transform(v.begin(), v.end(), std::back_inserter(v2), [](auto& p1) { return P2{p1.a, p1.c, p1.b}; });
+  std::cout << "sorting a vector<P2> in " << benchmark([&] {
+    std::sort(
+      v1.begin(),
+      v1.end(),  //
+      [](auto& e1, auto& e2) { return e1.b < e2.b; }
+    );
+  }).count() << " s\n";
+
+}
